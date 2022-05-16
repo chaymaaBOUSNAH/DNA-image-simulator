97c17b72fb674aaa68faced891bcc20f06d93551
Chaymaa_bs <chaimaabousnah@gmail.com>
2022-05-16 17:13:57 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import re
 
 def canvas2rgb_array(canvas):
     
@@ -17,3 +18,8 @@ def sorted_file( l ):
     convert = lambda text: int(text) if text.isdigit() else text 
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
+
+
+def distance(P1, P2):
+    dist = math.sqrt((P1[0]-P2[0])**2+(P1[1]-P2[1])**2)
+    return dist
