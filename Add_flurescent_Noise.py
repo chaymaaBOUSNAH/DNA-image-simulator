614aac018746d15e31701cdf7c63c2da88486a9d
Chaymaa_bs <chaimaabousnah@gmail.com>
2022-05-17 17:33:02 +0200

    Update Add_flurescent_Noise.py

--- Add_flurescent_Noise.py
+++ Add_flurescent_Noise.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import random
 from PIL import Image
 from utils import sorted_file, canvas2rgb_array
-
+from Draw_curves import draw_cercle
 
 '''
 fonction qui retourne une figure avec bruit coloré et flurescent
@@ -26,27 +26,29 @@ def Add_Flurescent_noise(image, Flurescent_noise, pepper):
         # autre bruit : poussière
         x = np.random.uniform(0, 2048)
         y = np.random.uniform(0, 2048)
-        colors = ['#32CD32', '#FF00FF']
-        color = random.choices(colors, weights=[0.8, 0.2])
+        colors = ['#00FF00', '#FF00FF', 'b']
+        color = random.choices(colors, weights=[0.8, 0.1, 0.01])
         alpha_value = 0.3
         
         n_point =  np.random.randint(10, 50)
         s = np.random.uniform(0.01, 10)
-            
+         
         for n in range(1, n_point+1):
             if n<5:
                 plt.scatter(x,y, marker='o', c = 'w', s = s*n, alpha = alpha_value/n)
             else:
                 plt.scatter(x,y, marker='o',color = color, s = s*n , alpha = alpha_value/(n*1.5))
+   
+
            
-    colors = ['#32CD32', '#FF34B3']
-    color = np.random.choice(colors, 1, p = [0.6, 0.4])
+    #colors = ['#32CD32', '#FF34B3']
+    #color = np.random.choice(colors, 1, p = [0.6, 0.4])
     for j in range(pepper):
         x = np.random.uniform(0, 2048)
         y = np.random.uniform(0, 2048)
-        size = np.random.uniform(1, 10)
-        #colors = ['#32CD32', '#FF34B3']
-        #color = np.random.choice(colors, 1, p = [0.7, 0.3])
+        size = np.random.uniform(0.1, 10)
+        colors = ['#00FF00', '#FF34B3', 'b']
+        color = np.random.choice(colors, 1, p = [0.89, 0.1, 0.01])
         markers = ['s', 'o']
         markr = np.random.choice(markers, 1, p=[0.6, 0.4])
         # BLUE #0000FF
@@ -56,21 +58,21 @@ def Add_Flurescent_noise(image, Flurescent_noise, pepper):
         # maroon1 #FF34B3 (255,52,179)
         # limegreen #32CD32 RGB(50,205,50)
         
-        #plt.scatter(x, y, s=size, c=color[0], marker=markr[0])    
+        plt.scatter(x, y, s=size, c=color[0], marker=markr[0])    
     ax.set_xlim((0, image.shape[1]))
     ax.set_ylim((image.shape[0], 0))
      
      
     return fig
 
-'''
-Flurescent_noise = np.random.randint(50, 200)
-pepper = np.random.randint(6000, 12000)
-image_path = './create_images/Essai/1.png'
 
-fig = Add_Flurescent_noise(image_path, Flurescent_noise, pepper)
+Flurescent_noise = np.random.randint(50, 200)
+pepper = np.random.randint(20000, 40000)
+image_path = './Essai/image_6.png'
+image = plt.imread(image_path)
+fig = Add_Flurescent_noise(image, Flurescent_noise, pepper)
 
 img = canvas2rgb_array(fig.canvas)
 pil_image=Image.fromarray(img)
 pil_image.show()
-'''
+pil_image.save('./Essai/image_6_noisy.png')
