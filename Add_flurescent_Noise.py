d38432b39d36c0df5e9d8960d946a821a50b75c8
Chaymaa_bs <chaimaabousnah@gmail.com>
2022-05-19 17:07:37 +0200

    Update Add_flurescent_Noise.py

--- Add_flurescent_Noise.py
+++ Add_flurescent_Noise.py
@@ -26,12 +26,12 @@ def Add_Flurescent_noise(image, Flurescent_noise, pepper):
         # autre bruit : poussière
         x = np.random.uniform(0, 2048)
         y = np.random.uniform(0, 2048)
-        colors = ['#00FF00', '#FF00FF', 'b']
-        color = random.choices(colors, weights=[0.8, 0.1, 0.01])
+        colors = ['#00FF00', '#FF00FF']
+        color = random.choices(colors, weights=[0.8, 0.2])
         alpha_value = 0.3
         
-        n_point =  np.random.randint(10, 50)
-        s = np.random.uniform(0.01, 10)
+        n_point =  np.random.randint(10, 30)
+        s = np.random.uniform(0.01, 7)
          
         for n in range(1, n_point+1):
             if n<5:
@@ -40,15 +40,15 @@ def Add_Flurescent_noise(image, Flurescent_noise, pepper):
                 plt.scatter(x,y, marker='o',color = color, s = s*n , alpha = alpha_value/(n*1.5))
    
 
-           
+    '''       
     #colors = ['#32CD32', '#FF34B3']
     #color = np.random.choice(colors, 1, p = [0.6, 0.4])
     for j in range(pepper):
         x = np.random.uniform(0, 2048)
         y = np.random.uniform(0, 2048)
-        size = np.random.uniform(0.1, 10)
-        colors = ['#00FF00', '#FF34B3', 'b']
-        color = np.random.choice(colors, 1, p = [0.89, 0.1, 0.01])
+        size = np.random.uniform(0.01, 5)
+        colors = ['#00FF00', 'r', 'b']
+        color = np.random.choice(colors, 1, p = [0.5, 0.2, 0.3])
         markers = ['s', 'o']
         markr = np.random.choice(markers, 1, p=[0.6, 0.4])
         # BLUE #0000FF
@@ -61,14 +61,14 @@ def Add_Flurescent_noise(image, Flurescent_noise, pepper):
         plt.scatter(x, y, s=size, c=color[0], marker=markr[0])    
     ax.set_xlim((0, image.shape[1]))
     ax.set_ylim((image.shape[0], 0))
-     
+    ''' 
      
     return fig
 
-
+'''
 Flurescent_noise = np.random.randint(50, 200)
-pepper = np.random.randint(20000, 40000)
-image_path = './Essai/image_6.png'
+pepper = np.random.randint(1000, 3000)
+image_path = './Essai/image_6_.png'
 image = plt.imread(image_path)
 fig = Add_Flurescent_noise(image, Flurescent_noise, pepper)
 
@@ -76,3 +76,4 @@ img = canvas2rgb_array(fig.canvas)
 pil_image=Image.fromarray(img)
 pil_image.show()
 pil_image.save('./Essai/image_6_noisy.png')
+'''
