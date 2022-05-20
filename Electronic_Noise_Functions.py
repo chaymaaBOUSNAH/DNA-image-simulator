dfbf2a142270268c07296d1c7b2c6345e42a7f00
Chaymaa_bs <75644760+chaymaaBOUSNAH@users.noreply.github.com>
2022-05-20 15:57:12 +0200

    Add files via upload

---
+++ Electronic_Noise_Functions.py
@@ -0,0 +1,106 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from os import walk
+from os.path import join
+import random
+
+
+
+def Gaussian_noise(image, sigma):
+   row,col=  image.shape
+   mean = 0
+   "Draw random samples from a normal (Gaussian) distribution."
+   # row * col * ch samples are drawn
+   gauss = np.random.normal(mean,sigma,(row,col)) 
+   gauss = gauss.reshape(row,col)
+   noisy = image + gauss
+   return noisy
+
+def Gaussian_noise_RGB(image, sigma):
+
+   row,col,ch= image.shape
+   mean = 0# Mean (“centre”) of the distribution.
+
+   sigma = np.random.uniform(0.2, 0.7)
+   "Draw random samples from a normal (Gaussian) distribution."
+   # row * col * ch samples are drawn
+   gauss = np.random.normal(mean,sigma,(row,col,ch)) 
+   gauss = gauss.reshape(row,col,ch)
+   noisy = image + gauss
+   return noisy
+
+
+
+def Add_Salt(image, amount):
+    row,col=  image.shape
+    # pourcentage de bruit salt& pepper à ajouter
+    # Add Salt 
+    # ceil return the smallest integer of a float 
+    num_salt = np.ceil(amount * image.size)
+    
+    # choisir aléatopirement les coordonnées ou on va mettre salt 
+    coord = [np.random.randint(0, i-1, int(num_salt)) for i in image.shape]
+    
+    x, y = coord
+    
+
+    #coords = np.array(coord)
+    image[(x, y)] = 255
+    
+    return image
+
+
+def degraded_fibers(image, sigma):
+    
+    output = image.copy()
+
+    row,col,ch= output.shape
+    
+    output_gaussian = Gaussian_noise_RGB(output, sigma)
+    output_gaussian  = np.uint8(np.clip(output_gaussian, 0, 255))
+    red_channel = output_gaussian[:, :, 0]
+    green_channel = output_gaussian[:, :, 1]
+    blue_channel = output_gaussian[:, :, 2]
+  
+    black = np.array([0], dtype='uint8')
+    
+    red_channel[red_channel <255] = black 
+    # or use np.where(red_channel <200, 0, red_channel)
+    green_channel[green_channel <255] = black
+    blue_channel[blue_channel <250] = black
+    
+    result_image = np.dstack((red_channel, green_channel, blue_channel))
+        
+    return result_image
+
+
+def Add_glow(image, glue_dir, prob):
+    row,col,ch= image.shape
+    imag = Image.fromarray(np.uint8(np.clip(image, 0, 255)))
+    Imagecopy = imag.copy() 
+    
+    dir_path = glue_dir
+    for (dirpath, dirnames, file_names) in walk(dir_path):
+        # n est le nombre aléatoire d'image de glue qui seront ajouter
+        add_glue = ['true', 'false']
+        adding_glue = random.choices(add_glue, weights=[prob, 1-prob])
+        if adding_glue==['true']:
+            n = np.random.randint(1, 3)
+            random_glue = random.sample(file_names, n)
+            for glue in random_glue:
+                
+                glue_h = np.random.randint(20, 150) 
+                glue_w = np.random.randint(20, 150) 
+                glue_path = join(dir_path, glue)
+                glue_img = Image.open(glue_path)
+                
+                #angle = random.choice([0, 90, 180])
+                #glue_img = glue_img.rotate(angle)
+                gluecopy = glue_img.copy() 
+                
+                a = np.random.randint(0, row-glue_h)
+                b = np.random.randint(0, col-glue_w)
+                
+                Imagecopy.paste(gluecopy.resize((glue_h, glue_w)), (a, b))
+    return np.array(Imagecopy) 
