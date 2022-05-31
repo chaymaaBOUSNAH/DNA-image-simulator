dc3ed0992692b6d240e774df834304e98622b5c1
Chaymaa_bs <chaimaabousnah@gmail.com>
2022-05-31 17:01:17 +0200

    Update Add_flurescent_Noise.py

--- Add_flurescent_Noise.py
+++ Add_flurescent_Noise.py
@@ -6,12 +6,13 @@ import random
 from PIL import Image
 from utils import sorted_file, canvas2rgb_array
 from Draw_curves import draw_cercle
-
+import time
 '''
 fonction qui retourne une figure avec bruit coloré et flurescent
 '''
 def Add_Flurescent_noise(image, Flurescent_noise, pepper):
-
+    start_time = time.time()
+    assert np.amax(image) <= 255 and np.amin(image) >= 0,  'values are not in the range [0 255], normalise image values'
     
     fig, ax = plt.subplots(1, 1, figsize=(2048, 2048), sharex=True, sharey=True, dpi=100)
     ax = plt.Axes(fig, [0., 0., 1., 1.])
@@ -41,39 +42,10 @@ def Add_Flurescent_noise(image, Flurescent_noise, pepper):
                 plt.scatter(x,y, marker='o',color = color, s = s*n , alpha = alpha_value/(n*1.5))
    
 
-    '''
-    #colors = ['#32CD32', '#FF34B3']
-    #color = np.random.choice(colors, 1, p = [0.6, 0.4])
-    for j in range(pepper):
-        x = np.random.uniform(0, 2048)
-        y = np.random.uniform(0, 2048)
-        size = np.random.uniform(4, 10)
-        markers = ['s', 'o']
-        markr = np.random.choice(markers, 1, p=[0.6, 0.4])
-        # BLUE #0000FF
-        # magenta #FF00FF: (255,0,255) # Aqua 00FFFF:(0,255,255)
-        # GREENYELLOW #ADFF2F (173,255,47)
-        # olivedrab1 #C0FF3E (192,255,62)
-        # maroon1 #FF34B3 (255,52,179)
-        # limegreen #32CD32 RGB(50,205,50)
-        
-        plt.scatter(x, y, s=size, c='#32CD32', marker=markr[0])    
-    '''
     ax.set_xlim((0, image.shape[1]))
     ax.set_ylim((image.shape[0], 0))
     
+    print('Flurescent noise', "--- %s seconds ---" % (time.time() - start_time))  
      
-    return fig
+    return canvas2rgb_array(fig.canvas)
 
-'''
-Flurescent_noise = np.random.randint(50, 200)
-pepper = np.random.randint(1000, 3000)
-image_path = './Essai/image_0_1.png'
-image = plt.imread(image_path)
-fig = Add_Flurescent_noise(image, Flurescent_noise, pepper)
-
-img = canvas2rgb_array(fig.canvas)
-pil_image=Image.fromarray(img)
-pil_image.show()
-
-'''
